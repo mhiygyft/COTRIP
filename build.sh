558543861149bef73c1deb541ab65094d3428bb8
mhiygyft <meossthienanhs@gmail.com>
2026-05-28 03:03:27 +0700

    Auto import production database

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
+# #!/usr/bin/env bash
+# set -o errexit
+
+# python -m pip install --upgrade pip
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --noinput
+# python manage.py migrate --noinput
+
+# python manage.py seed_transport_data
+# python manage.py seed_multicity_flights
+# python manage.py seed_web_travel_content
+# python manage.py seed_youth_homestays_hotspots
+# python manage.py seed_real_image_urls
 #!/usr/bin/env bash
 set -o errexit
 
@@ -12,3 +26,5 @@ python manage.py seed_multicity_flights
 python manage.py seed_web_travel_content
 python manage.py seed_youth_homestays_hotspots
 python manage.py seed_real_image_urls
+
+python manage.py loaddata data.json || true
